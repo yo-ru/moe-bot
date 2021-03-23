415b3a04a039d56171c662a0866e7eab40e8e138
yo-ru <32829880+Yo-ru@users.noreply.github.com>
2021-03-23 12:28:08 -0500

    music

---
+++ cogs/music.py
@@ -0,0 +1,85 @@
+import asyncio
+from cmyui import Ansi, log
+from youtube_dl import YoutubeDL
+from discord.ext.commands import Cog
+from discord_slash import SlashContext, cog_ext
+from discord import Embed, FFmpegPCMAudio, PCMVolumeTransformer
+
+import config
+
+"""
+youtube_dl - our source for audio played in voice channels.
+"""
+youtube_dl.utils.bug_reports_message = lambda: ''
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
+}
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = YoutubeDL(ytdl_format_options)
+class YTDLSource(PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+
+"""
+Music - a cog designed to play music.
+"""
+class Music(Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    """
+    play - play a specified YouTube video in the currently connected voice channel.
+    """
+    @cog_ext.cog_slash(
+        name="play",
+        description="Play a YouTube video in a voice channel!",
+        guild_ids=config.guild_ids
+    )
+    async def _play(self, ctx: SlashContext, URL: str) -> SlashContext:
+        await ctx.respond()
+
+        # if bot is already connected, move them.
+        channel = ctx.author.voice.channel
+        if ctx.voice_client:
+            await ctx.voice_client.move_to(channel)
+        else:
+            await channel.connect()
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(URL, loop=self.bot.loop, stream=True)
+            ctx.voice_client.play(player, after=ctx.voice_client.disconnect())
+        await ctx.send(f"Now playing: **{player.title}**")
+
+def setup(bot) -> None:
+    bot.add_cog(Music(bot))
