a9b921d9dabbfa6c8606e93aff8c04357b36e0e2
Yo-ru <32829880+yo-ru@users.noreply.github.com>
2022-08-23 14:02:18 -0500

    TEST: fix datetime

--- cogs/skittle.py
+++ cogs/skittle.py
@@ -1,5 +1,5 @@
-import time
 import nextcord
+from datetime import datetime
 from nextcord.ext.commands import Cog
 from nextcord.ext import application_checks
 from nextcord import Embed, Interaction, SlashOption
@@ -98,7 +98,7 @@ class Skittle(Cog):
                 )
                 embed.add_field(name="Payment Status", value=status["status"]["status"], inline=True)
                 embed.add_field(name="Payment Amount", value=f"{payment['gateway']['data']['total']['base']} USD", inline=True)
-                embed.add_field(name="Payment Date", value=time(status["status"]["updatedAt"]).strftime("%H:%M:%S - %B %d, %Y"), inline=True)
+                embed.add_field(name="Payment Date", value=datetime.fromtimestamp(status["status"]["updatedAt"]).strftime("%H:%M:%S - %B %d, %Y"), inline=True)
                 embed.add_field(name="Order Email", value=customer_info["email"], inline=True)
                 embed.add_field(name="Order IP", value=customer_info["ip"], inline=True)
                 embed.add_field(name="Product URL", value=product_urls, inline=True)
